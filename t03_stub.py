af93931caea11f8a281a9954aee1b7a2a99301c8
nguyent2 <nguyent2@berea.edu>
2019-09-11 10:04:32 -0400

    Updating the complete project by Thy - Jhonny was absent.

--- t03_stub.py
+++ t03_stub.py
@@ -23,7 +23,7 @@ jhonny.color("yellow")
 thy.penup()
 thy.setposition(-250,-250)
 jhonny.penup()
-jhonny.setposition(-224, -224)
+jhonny.setposition(-230, -230)
 def function_1():
     """
     This functions draw a square outside
@@ -34,18 +34,33 @@ def function_1():
         thy.forward(501)
         thy.left(90)
 
+
+def forward(th):
+    """
+    This function draws the last line of the Boustrophedon
+    :param th:
+    :return:
+    """
+    th.forward(461)
 def function_2():
     """
-    This functions fills in the square with Boustrophedon
+    This functions fills the inside of the square with Boustrophedon
     """
     pass
 
     jhonny.pendown()
-    jhonny.forward(480)
-    jhonny.left(90)
-    jhonny.forward
-    jhonny.forward(480)
-    jhonny.right(90)
+    for i in range(int(501/38-1)): #Since 1 loop is 2 lines, which is the pensize is 19*2 = 38
+        jhonny.forward(461)
+        jhonny.left(90)
+        jhonny.forward(19)
+        jhonny.left(90)
+        jhonny.forward(461)
+        jhonny.right(90)
+        jhonny.forward(19)
+        jhonny.right(90)
+    forward(jhonny)
+
+
 
 
 def main():
